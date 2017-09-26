a9759686e011395972c3116024826e4429636ebf
TiagoFornelos <t.fornelos@campus.fct.unl.pt>
2017-09-26 20:47:47 +0100

    asaaaaaa

--- homeAway/PropertyClass.java
+++ homeAway/PropertyClass.java
@@ -10,25 +10,27 @@ public class PropertyClass implements PropertyWritable {
 	int price;
 	int maxPersons;
 	int points;
-	
-	
-	public PropertyClass(String idHome, String idUser, int price, int maxPersons, String local, String description, String adress) {
-		this.idHome=idHome;
-		this.owner=idUser;
-		this.price=price;
-		this.maxPersons=maxPersons;
+
+	public PropertyClass(String idHome, User idUser, int price, int maxPersons, String local, String description,
+			String adress) {
+		this.idHome = idHome;
+		this.owner = idUser;
+		this.price = price;
+		this.maxPersons = maxPersons;
+		this.local=local;
+		this.description= description;
+		this.adress = adress;
 	}
-	
-	
+
 	@Override
 	public String getIdHome() {
-		
+
 		return idHome;
 	}
 
 	@Override
 	public String getAdress() {
-	
+
 		return adress;
 	}
 
@@ -39,31 +41,31 @@ public class PropertyClass implements PropertyWritable {
 
 	@Override
 	public String getLocal() {
-		
+
 		return local;
 	}
 
 	@Override
 	public int getPrice() {
-		
+
 		return price;
 	}
 
 	@Override
 	public int getMaxPersons() {
-		
+
 		return maxPersons;
 	}
 
 	@Override
 	public User getOwner() {
-		
+
 		return owner;
 	}
 
 	@Override
 	public int getPoints() {
-		
+
 		return points;
 	}
 

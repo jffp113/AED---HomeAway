9c17c1439e645b39314f05b7e89547477311275a
jffp113 <jff.pereira@campus.fct.unl.pt>
2017-10-26 15:02:32 +0100

    Lista

--- dataStructures/OrderedDoubleList.java
+++ dataStructures/OrderedDoubleList.java
@@ -6,187 +6,208 @@ package dataStructures;
  * @param <K>
  * @param <V>
  */
-public class OrderedDoubleList<K extends Comparable<K>, V> implements OrderedDictionary<K, V> {
+public class OrderedDoubleList<K extends Comparable<K>,V> implements OrderedDictionary<K, V> {
+	//Constants
+	
 
 	private static final long serialVersionUID = 1L;
+	
+	//Variables
+	protected DListNode<Entry<K,V>> head;
+	protected DListNode<Entry<K,V>> tail;
 	protected int currentSize;
-	protected DListNode<Entry<K, V>> head;
-	protected DListNode<Entry<K, V>> tail;
-
+	
+	//Constructor
 	public OrderedDoubleList() {
 		head = null;
 		tail = null;
 		currentSize = 0;
+		
 	}
-
+	
+	
 	@Override
 	public boolean isEmpty() {
-
 		return currentSize == 0;
 	}
 
 	@Override
 	public int size() {
-
 		return currentSize;
 	}
 
-	protected DListNode<Entry<K, V>> searchSpot(K key) {
-		DListNode<Entry<K, V>> s = head;
-
-		if (this.isEmpty())
-			return null;
-
-		while (s.getNext() != null && s.getElement().getKey().compareTo(key) <= -1)
-			s = s.getNext();
-
-		return s;
-	}
-
+	
 	@Override
-
 	public V find(K key) {
-		DListNode<Entry<K, V>> node = searchSpot(key);
-		if (node == null)
+		DListNode<Entry<K,V>> next = searchSpot(key);
+		
+		if(next == null)
 			return null;
-		else if (key.equals(node.getElement().getKey()))
-			return node.getElement().getValue();
-		else
+		else if(next.getElement().getKey().compareTo(key) == 0)
+			return next.getElement().getValue();
+		else		
 			return null;
 	}
 
 	protected void insertFirst(K key, V value) {
-		DListNode<Entry<K, V>> node = new DListNode<Entry<K, V>>(new EntryClass<K, V>(key, value), null, head);
-
-		if (head == null)
+		DListNode<Entry<K,V>> node = new DListNode<Entry<K,V>>(new EntryClass<K,V>(key,value),null,head); 
+				
+		if(head == null)
 			tail = node;
-		else
+		else 
 			head.setPrevious(node);
-
+		
 		head = node;
 		currentSize++;
-
+		
 	}
-
+	
 	protected void insertLast(K key, V value) {
-		DListNode<Entry<K, V>> node = new DListNode<Entry<K, V>>(new EntryClass<K, V>(key, value), tail, null);
-
-		if (tail == null)
+		DListNode<Entry<K,V>> node = new DListNode<Entry<K,V>>(new EntryClass<K,V>(key,value),tail,null);
+		
+		if(tail == null)
 			head = node;
 		else
 			tail.setNext(node);
-
+		
 		tail = node;
 		currentSize++;
 	}
-
+	
 	protected V insertMiddle(K key, V value) {
-		DListNode<Entry<K, V>> s = searchSpot(key);
-		DListNode<Entry<K, V>> newNode;
 		V oldV = null;
-		if (key.equals(s.getElement().getKey())) {
+		DListNode<Entry<K,V>> s = searchSpot(key);
+		DListNode<Entry<K,V>> node;
+		
+		if(s.getElement().getKey().compareTo(key) == 0) {//If the key is present substitute with new vaule
 			oldV = s.getElement().getValue();
-			s.setElement(new EntryClass<K, V>(key, value));
-		} else {
-			newNode = new DListNode<Entry<K, V>>(new EntryClass<K, V>(key, value), s.getPrevious(), s);
-			if (s.getPrevious() != null)
-				s.getPrevious().setNext(newNode);
-			s.setPrevious(newNode);
-			currentSize++;
+			s.setElement(new EntryClass<K,V>(key,value));
 		}
+		else {
+			node = new DListNode<Entry<K,V>>(new EntryClass<K,V>(key,value),s.getPrevious(),s);
+			if(s.getPrevious() != null)
+				s.getPrevious().setNext(node);
+			s.setPrevious(node);
+			this.currentSize++;
+		}
+		
+		
 		return oldV;
 	}
-
+	
+	/**
+	 * Search the element or the nears element to that
+	 * key
+	 * @param key
+	 * @return
+	 */
+	protected DListNode<Entry<K,V>> searchSpot(K key) {
+		DListNode<Entry<K,V>> s = head;
+		
+		if(this.isEmpty())
+			return null;
+		
+		while(s.getNext() != null && s.getElement().getKey().compareTo(key) <= -1)
+			s = s.getNext();
+		
+		return s;
+	}
+	
 	@Override
-
 	public V insert(K key, V value) {
-		if (currentSize == 0 || head.getElement().getKey().compareTo(key) >= 1)
-			insertFirst(key, value);
-		else if (tail.getElement().getKey().compareTo(key) <= -1)
+		V oldV = null;
+		
+		if(currentSize == 0 || head.getElement().getKey().compareTo(key) >= 1)//if there are no elements
+			insertFirst(key,value);
+		else if(tail.getElement().getKey().compareTo(key) <= -1)//if key is higher that
 			insertLast(key, value);
-		else
-			return insertMiddle(key, value);
-		return null;
-
+		else 													//if the element must be in the middle
+			oldV = insertMiddle(key,value);
+		
+		return oldV;
 	}
 
 	/**
 	 * Removes first element
-	 * 
 	 * @requires list must not be empty
 	 */
 	protected void removeFirst() {
 		head = head.getNext();
-
-		if (head == null)
+		
+		if(head == null)
 			tail = null;
 		else
 			head.setPrevious(null);
-
+		
 		this.currentSize--;
 	}
-
+	
 	/**
 	 * Removes last element
-	 * 
 	 * @requires list must not be empty
 	 */
 	protected void removeLast() {
 		tail = tail.getPrevious();
-
-		if (tail == null)
+		
+		if(tail == null)
 			head = null;
 		else
 			tail.setNext(null);
-
+		
 		this.currentSize--;
-
+		
 	}
-
-	protected void removeMiddle(DListNode<Entry<K, V>> node) {
-		DListNode<Entry<K, V>> next = node.getNext();
-		DListNode<Entry<K, V>> previous = node.getPrevious();
+	
+	/**
+	 * Removes middle node
+	 * @param elem node where must be placed near or substitute
+	 */
+	protected void removeMiddle(DListNode<Entry<K, V>> elem) {
+		DListNode<Entry<K,V>> next = elem.getNext();
+		DListNode<Entry<K,V>> previous = elem.getPrevious();
 		next.setPrevious(previous);
 		previous.setNext(next);
-
+		
 		this.currentSize--;
-
+		
 	}
-
+	
 	@Override
 	public V remove(K key) {
-		V value = null;
-		DListNode<Entry<K, V>> node = searchSpot(key);
-		if (node == null)
+		DListNode<Entry<K,V>> elem = this.searchSpot(key);
+		V old = null;
+		
+		if(elem == null)
 			return null;
-		else if (node.getElement().getKey().equals(key)) {
-			value = node.getElement().getValue();
-			if (node == head)
+		
+		else if(elem.getElement().getKey().compareTo(key) == 0) {
+			old = elem.getElement().getValue();
+			
+			if(elem == head)
 				removeFirst();
-			else if (node == tail)
+			else if (elem == tail)
 				removeLast();
 			else
-				removeMiddle(node);
+				removeMiddle(elem);
 		}
-		return value;
-
+		return old;
 	}
+
 
 	@Override
 	public Iterator<Entry<K, V>> iterator() {
-		return new DoublyLLIterator<>(head, tail);
+		return new DoublyLLIterator<Entry<K,V>>(head, tail);
 	}
 
-	@Override
-	public Entry<K, V> minEntry() throws EmptyDictionaryException {
 
+	@Override
+	public Entry<K, V> minEntry() {
 		return head.getElement();
 	}
 
-	@Override
-	public Entry<K, V> maxEntry() throws EmptyDictionaryException {
-		// TODO Auto-generated method stub
-		return tail.getElement();
-	}
 
+	@Override
+	public Entry<K, V> maxEntry() {
+		return  tail.getElement();
+	}
 }
